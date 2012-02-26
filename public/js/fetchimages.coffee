d3fc0684d55e376827640d9cbb1b190c6bab73d5
Alex Godin <alex@alexgodin.com>
2012-02-25 23:10:26 -0500

    funkyness

--- public/js/fetchimages.coffee
+++ public/js/fetchimages.coffee
@@ -2,7 +2,7 @@ $(document).ready ->
   $('.left .btn').click =>
     $.ajax({ url :"/search/#{$('.left input')[0].value}", async : true, crossDomain : true, success : (results) =>
       for picture in JSON.parse(results)
-        $(".left #images").append("<img id='results' src='#{picture.thumb_url}' data-url='#{picture.url}'>")
+        $(".left #images").append("<img id='results' src='#{picture.thumb_url}' data-url='#{picture.url}' data-id='#{picture.image_id}'>")
       for element in $(".left #images img")
         $(element).bind 'click', (event) =>
           $(event.target).toggleClass("selected")
@@ -10,6 +10,15 @@ $(document).ready ->
   $('.right .btn').click =>
     $.ajax({ url :"/search/#{$('.right input')[0].value}", async : true, crossDomain : true, success : (results) =>
       for picture in JSON.parse(results)
-        $(".right #images").append("<img id='results' src='#{picture.thumb_url}'>")
+        $(".right #images").append("<img id='results' src='#{picture.thumb_url}' data-url='#{picture.url}' data-id='#{picture.image_id}'>")
+      for element in $(".right #images img")
+        $(element).bind 'click', (event) =>
+          $(event.target).toggleClass("selected")
     })
+  $('.btn-danger').click =>
+    $.ajax({ url :"/foo/#{$('.left .selected').data('id')}", async : true, crossDomain : true, success : (results) =>
+      photoId = JSON.parse(results)['photo_id']
+      console.log(photo_id)
+
+
 
--- public/js/fetchimages.js
+++ public/js/fetchimages.js
@@ -4,7 +4,7 @@
     var _this = this;
     $('.left .btn').click(function() {
       return $.ajax({
-        url: "http://192.168.2.2:4567/search/" + ($('.left input')[0].value),
+        url: "/search/" + ($('.left input')[0].value),
         async: true,
         crossDomain: true,
         success: function(results) {
@@ -12,7 +12,7 @@
           _ref = JSON.parse(results);
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             picture = _ref[_i];
-            $(".left #images").append("<img id='results' src='" + picture.thumb_url + "' data-url='" + picture.url + "'>");
+            $(".left #images").append("<img id='results' src='" + picture.thumb_url + "' data-url='" + picture.url + "' data-id='" + picture.image_id + "'>");
           }
           _ref2 = $(".left #images img");
           _results = [];
@@ -28,16 +28,23 @@
     });
     return $('.right .btn').click(function() {
       return $.ajax({
-        url: "http://192.168.2.2:4567/search/" + ($('.right input')[0].value),
+        url: "/search/" + ($('.right input')[0].value),
         async: true,
         crossDomain: true,
         success: function(results) {
-          var picture, _i, _len, _ref, _results;
+          var element, picture, _i, _j, _len, _len2, _ref, _ref2, _results;
           _ref = JSON.parse(results);
-          _results = [];
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             picture = _ref[_i];
-            _results.push($(".right #images").append("<img id='results' src='" + picture.thumb_url + "'>"));
+            $(".right #images").append("<img id='results' src='" + picture.thumb_url + "' data-url='" + picture.url + "' data-id='" + picture.image_id + "'>");
+          }
+          _ref2 = $(".right #images img");
+          _results = [];
+          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+            element = _ref2[_j];
+            _results.push($(element).bind('click', function(event) {
+              return $(event.target).toggleClass("selected");
+            }));
           }
           return _results;
         }
